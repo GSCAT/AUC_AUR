da6982b95cdeb60ac1d6c243a50092641adb8091
Kevin Bonds <kevin.w.bonds@gmail.com>
2017-01-30 14:45:33 -0800

    Add code to save NOVA_data

--- NOVA.R
+++ NOVA.R
@@ -1,9 +1,20 @@
 library(dplyr)
 library(readr)
 library(RODBC)
+library(rChoiceDialogs)
 
+# Read in passwords ----
 my_uid <- read_lines("C:\\Users\\Ke2l8b1\\Documents\\my_uid.txt")
 my_pwd <- read_lines("C:\\Users\\Ke2l8b1\\Documents\\my_pwd.txt")
+
+# Function for opening file chooser ----
+choose_file_directory <- function()
+{
+  v <- jchoose.dir()
+  return(v)
+}
+
+my_directory <- choose_file_directory()
 
 # Create RODBC connection---- 
 my_connect <- odbcConnect(dsn= "IP EDWP", uid= my_uid, pwd= my_pwd)
@@ -11,4 +22,6 @@ my_connect <- odbcConnect(dsn= "IP EDWP", uid= my_uid, pwd= my_pwd)
 sqlQuery(my_connect, query = "SELECT  * from dbc.dbcinfo;")
 
 NOVA_data <- sqlQuery(my_connect, query = "SELECT * FROM SRAA_SAND.NOVA_ACTL_YTD")
+
+save(NOVA_data, file = paste(my_directory, "NOVA_DATA.rda", sep = .Platform$file.sep ))
 
